4d3b0c864389f9b765587c28f8af69cb198390c2
venkataramireddyyarram <s554991@nwmissouri.edu>
2022-11-17 01:11:41 -0600

    updated

--- controllers/water.js
+++ controllers/water.js
@@ -1,84 +1,154 @@
-var water = require('../models/water');
-// List of all waters
-exports.water_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: water list');
-};
-// for a specific water.
-exports.water_detail = async function(req, res) {
-    console.log("detail"  + req.params.id)
-    try {
-        result = await water.findById( req.params.id)
-        res.send(result)
-    } catch (error) {
-        res.status(500)
-        res.send(`{"error": document for id ${req.params.id} not found`);
-    }
-};
-
-// Handle water create on POST.
-exports.water_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new water();
+var water = require('../models/water'); 
+ 
+// List of all water 
+exports.water_list = async function(req, res) { 
+    try{ 
+        thewaters = await water.find(); 
+        res.send(thewaters); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ // VIEWS 
+// Handle a show all view 
+exports.water_view_all_Page = async function(req, res) { 
+    try{ 
+        thewaters = await water.find(); 
+        res.render('water', { title: 'water Search Results', results: thewaters }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+// Handle water create on POST. 
+exports.water_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new water(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"Earp_Name":"Applepods", "Earp_Type":"Medium", "Earp_Size":3.5} 
     document.Water_Name = req.body.Water_Name;
     document.Water_Company = req.body.Water_Company;
     document.Water_cost = req.body.Water_cost;
-    document.Water_Rating = req.body.Water_Rating;
-    try{
-        let result = await document.save();
-        res.send(result);
-    }
-    catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }  
-};
-// Handle water delete form on DELETE.
-exports.water_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: water delete DELETE ' + req.params.id);
-};
-// Handle water update form on PUT.
-exports.water_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: water update PUT' + req.params.id);
-};
-exports.water_list = async function(req, res) {
-    try{
-        thewaters = await water.find();
-        res.send(thewaters);
-    }
-    catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }  
-};
-// Handle Water update form on PUT.
-exports.water_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.id} with body
-${JSON.stringify(req.body)}`)
-    try {
-        let toUpdate = await water.findById( req.params.id)
-        // Do updates of properties
+    document.Water_Rating = req.body.Water_Rating; 
+
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
+
+
+// for a specific water. 
+exports.water_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await water.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+
+
+// Handle water update form on PUT. 
+exports.water_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await water.findById( req.params.id) 
+        // Do updates of properties 
         if(req.body.Water_Name)
                toUpdate.Water_Name = req.body.Water_Name;
-        if(req.body.cost) toUpdate.cost = req.body.cost;
-        if(req.body.Rating) toUpdate.Rating = req.body.Rating;
-        let result = await toUpdate.save();
-        console.log("Sucess " + result)
-        res.send(result)
-    } catch (err) {
-        res.status(500)
-        res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
-    }
-};
-// VIEWS
-// Handle a show all view
-exports.water_view_all_Page = async function(req, res) {
-    try{
-        thewaters = await water.find();
-        res.render('water', { title: 'water Search Results', results: thewaters });
-    }
-    catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }  
-};
+        if(req.body.Water_Company) toUpdate.Water_Company = req.body.Water_Company;
+        if(req.body.Water_cost) toUpdate.Water_cost = req.body.Water_cost;
+        if(req.body.Water_Rating) toUpdate.Water_Rating = req.body.Water_Rating;
+
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
+}; 
+  
+
+
+// Handle water delete on DELETE. 
+exports.water_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await water.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+// Handle a show one view with id specified by query 
+exports.water_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await water.findById( req.query.id) 
+        res.render('waterdetail',  { title: 'water Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for creating a water. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.water_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('watercreate', { title: 'water Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a water. 
+// query provides the id 
+exports.water_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await water.findById(req.query.id) 
+        res.render('waterupdate', { title: 'water Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.water_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await water.findById(req.query.id) 
+        res.render('waterdelete', { title: 'water Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/water.js
+++ routes/water.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var water_controller = require('../controllers/water');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('water', { title: 'search results for water class' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('water', { title: 'Search Results for water Class' });
+// });
+
+/* GET water */ 
+router.get('/', water_controller.water_view_all_Page ); 
+
+// GET request for one water. 
+router.get('/waters/:id', water_controller.water_detail);
+
+/* GET detail water page */ 
+router.get('/detail', water_controller.water_view_one_Page); 
+
+/* GET create water page */ 
+router.get('/create', water_controller.water_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', water_controller.water_update_Page);
+
+/* GET delete water page */ 
+router.get('/delete', water_controller.water_delete_Page); 
 
 module.exports = router;
